6a049f78e27d833c74028ba4b11810d92a66e9f8
AleksLabuta <al-eksandr@ya.ru>
2022-12-18 18:31:27 +0500

    day 2 task 1

--- main/java/day2/Task1.java
+++ main/java/day2/Task1.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner sc = new  Scanner(System.in);
+        int stage = sc.nextInt();
+        if (0 < stage && stage <=4 ){
+            System.out.println("Малоэтажный дом");
+        } else if (5 <= stage && stage <= 8){
+            System.out.println("Среднеэтажный дом");
+        } else if ( stage >= 9){
+            System.out.println("Многоэтажный дом");
+        } else{
+            System.out.println("Ошибка ввода");
+        }
     }
 }
